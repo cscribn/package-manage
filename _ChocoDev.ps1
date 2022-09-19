4320e3612b685f9b208d2b4bb30a8f1799983477
Chad Scribner <chadbscribner@gmail.com>
2022-09-19 15:33:48 -0500

    Node support

--- _ChocoDev.ps1
+++ _ChocoDev.ps1
@@ -1,6 +1,12 @@
 choco upgrade github-desktop -y
+
+# node
 choco upgrade nvm -y
 choco upgrade nvm.install -y
+nvm uninstall latest
+nvm install latest
+nvm use latest
+
 choco upgrade strawberryperl -y
 choco upgrade python -y
 choco upgrade xmlstarlet -y
