2259c0eb8a9c3e57ede29d29862fcfb76917b8a9
Chad Scribner <chadbscribner@gmail.com>
2022-09-21 21:04:29 -0500

    Updating python

--- _ChocoDev.ps1
+++ _ChocoDev.ps1
@@ -7,6 +7,14 @@ nvm install latest
 nvm use latest
 
 choco upgrade strawberryperl -y
+
+# python
 choco upgrade python -y
+$PythonPath = Resolve-Path "C:\Python3*\python.exe"
+$PythonWPath = Resolve-Path "C:\Python3*\pythonw.exe"
+Rename-Item -Path "$PythonPath" -NewName "python3.exe"
+Rename-Item -Path "$PythonWPath" -NewName "pythonw3.exe"
+choco upgrade python2 -y
+
 choco upgrade xmlstarlet -y
 choco upgrade vscode -y
