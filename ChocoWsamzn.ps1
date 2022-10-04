a48447d9d3127e5252d899a27059bfcd33fbfae0
Chad Scribner <chadbscribner@gmail.com>
2022-10-04 15:52:38 -0500

    Conemu config update

--- ChocoWsamzn.ps1
+++ ChocoWsamzn.ps1
@@ -1,7 +1,14 @@
 . $PSScriptRoot\_ChocoDefault.ps1
 . $PSScriptRoot\_ChocoDev.ps1
 
+# conemu
 choco upgrade conemu -y
+Set-Location "$Env:APPDATA"
+
+curl -Lo "ConEmu.xml" https://raw.githubusercontent.com/cscribn/config-misc/main/conemu/ConEmu.xml
+
+Set-Location -
+
 choco upgrade nvm -y
 choco upgrade postman -y
 
