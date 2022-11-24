239addc78e8d88f69fd1d1b7a5e29ca455e0347b
Chad Scribner <chadbscribner@gmail.com>
2022-11-23 22:19:27 -0600

    Non-apt at top

--- apt-pidns.sh
+++ apt-pidns.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Non-apt installations (add/update these first)
+# nextdns
+# sh -c 'sh -c "$(curl -sL https://nextdns.io/install)"'
+
 source ./_apt-default.sh
 
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt clean -y
-
-# manual: nextdns
-# sh -c 'sh -c "$(curl -sL https://nextdns.io/install)"'
