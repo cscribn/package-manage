e230c352fd59f3c7eb48be6604189c9b007a3651
Chad Scribner <chadbscribner@gmail.com>
2023-01-06 14:04:21 -0600

    Wondershaper and log removal

--- apt-retropie.sh
+++ apt-retropie.sh
@@ -18,10 +18,6 @@ fi
 # include
 source ./_apt-default.sh
 
-# wondershaper
-rm -rf ~/wondershaper
-git clone https://github.com/magnific0/wondershaper.git ~/wondershaper
-
 # RetroPie-Extra
 curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-duckstation.sh" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-duckstation.sh
 
@@ -31,7 +27,6 @@ curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro.sh
 
 curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro/01_shader_hack_rpi4.diff" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-yabasanshiro/01_shader_hack_rpi4.diff
 
-ls -t /home/pi/RetroPie-Setup/logs/*.gz | tail -n +2 | xargs rm --
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt clean -y
