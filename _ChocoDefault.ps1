c1795274672d10d33cac07c1ab2987fa1aa5eced
Chad Scribner <chadbscribner@gmail.com>
2023-01-13 15:34:24 -0600

    Still instead of fresh

--- _ChocoDefault.ps1
+++ _ChocoDefault.ps1
@@ -66,7 +66,7 @@ choco upgrade inkscape -y
 choco upgrade irfanview -y
 choco upgrade irfanviewplugins -y
 choco upgrade kitty -y
-choco upgrade libreoffice-fresh -y
+choco upgrade libreoffice-still -y
 choco upgrade lsd -y
 
 # Meslo LGS Nerd Font
