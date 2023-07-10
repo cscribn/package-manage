20f76e7a9792e57daa752ad1deeb0193900273d3
Chad Scribner <chadbscribner@gmail.com>
2023-07-10 08:19:16 -0500

    Adding terraform

--- brew-work.sh
+++ brew-work.sh
@@ -126,6 +126,11 @@ rbenv install -s 2.7.6
 brew install --cask slack || brew upgrade --cask slack
 brew install snyk || brew upgrade snyk
 brew install --cask studio-3t || brew upgrade --cask studio-3t
+
+# terraform
+brew install tfenv || brew upgrade tfenv
+tfenv install 1.5.2
+
 brew install --cask visual-studio-code || brew upgrade --cask visual-studio-code
 brew install vim || brew upgrade vim
 brew install --cask vlc || brew upgrade --cask vlc
