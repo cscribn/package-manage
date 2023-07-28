dc278380550a1b356de2662633a527d0e91bfef3
Chad Scribner <chadbscribner@gmail.com>
2023-07-28 14:41:20 -0500

    Adding hex editor

--- _ChocoDefault.ps1
+++ _ChocoDefault.ps1
@@ -75,6 +75,7 @@ choco upgrade gimp -y
 choco upgrade github-desktop -y
 choco upgrade guiformat -y
 choco upgrade handbrake -y
+choco upgrade hxd -y
 choco upgrade imagemagick.app -y
 choco upgrade imgburn -y
 choco upgrade inkscape -y
