b35f266aaef98252a671728bb4a367302ce1328e
Chad Scribner <chadbscribner@gmail.com>
2023-08-02 15:51:51 -0500

    Removing unneeded

--- ChocoSkinner.ps1
+++ ChocoSkinner.ps1
@@ -13,8 +13,6 @@ $StartTime = Get-Date
 choco upgrade barrier -y
 choco upgrade dvdflick-v2 -y
 choco upgrade makemkv -y
-choco upgrade nvidia-display-driver -y
-choco upgrade plexmediaserver -y
 choco upgrade rpi-imager -y
 choco upgrade win32diskimager --version 0.9.5 -y
 choco pin add --name="'win32diskimager'" --version="'0.9.5'"
