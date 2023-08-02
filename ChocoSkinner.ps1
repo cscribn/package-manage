f16b34396029b000175d7abb7c730ec4bbfca1c1
Chad Scribner <chadbscribner@gmail.com>
2023-08-02 15:46:53 -0500

    Changing to computer name

---
+++ ChocoSkinner.ps1
@@ -0,0 +1,29 @@
+# Non-Chocolatey installations (add/update these first)
+# alienware update universal
+# backblaze
+# bluestacks
+# iso compressor
+# papercut mobility print
+# samsung magician
+# skraperui
+
+$StartTime = Get-Date
+. $PSScriptRoot\_ChocoDefault.ps1
+
+choco upgrade barrier -y
+choco upgrade dvdflick-v2 -y
+choco upgrade makemkv -y
+choco upgrade nvidia-display-driver -y
+choco upgrade plexmediaserver -y
+choco upgrade rpi-imager -y
+choco upgrade win32diskimager --version 0.9.5 -y
+choco pin add --name="'win32diskimager'" --version="'0.9.5'"
+
+choco feature enable -n useRememberedArgumentsForUpgrades
+choco upgrade all -y
+
+$Desktops = "$env:PUBLIC\Desktop", "$env:USERPROFILE\Desktop"
+
+If ($null -ne $StartTime) {
+	$Desktops | Get-ChildItem -Filter "*.lnk" -ErrorAction SilentlyContinue | Where-Object { $_.LastWriteTime -gt $StartTime } | Remove-Item
+}
