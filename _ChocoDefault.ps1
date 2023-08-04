915f91a08b902a3ce79742ed27c716e2e3c007eb
Chad Scribner <chadbscribner@gmail.com>
2023-08-04 13:42:26 -0500

    Removing edge

--- _ChocoDefault.ps1
+++ _ChocoDefault.ps1
@@ -85,7 +85,6 @@ choco upgrade kitty --version 0.76.1.9 -y
 choco pin add --name="'kitty'" --version="'0.76.1.9'"
 choco upgrade libreoffice-still -y
 choco upgrade lsd -y
-choco upgrade microsoft-edge -y
 choco upgrade microsoft-teams -y
 choco upgrade mp3tag -y
 choco upgrade nextdns -y
