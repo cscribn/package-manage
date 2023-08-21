78a3afc0402867cff23ffc617f7fb7d975bc998e
Chad Scribner <chad-scribner@pluralsight.com>
2023-08-21 14:00:40 -0500

    Adding key bindings

--- _brew-pluralsight-config.sh
+++ _brew-pluralsight-config.sh
@@ -69,5 +69,20 @@ if [[ "$clone" = 1 ]]; then
 fi
 
 # pwsh
-mkdir "$HOME/.config/powershell"
-curl -Lo "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1" https://raw.githubusercontent.com/cscribn/config-misc/main/powershell-core/Microsoft.PowerShell_profile.ps1
+mkdir -p "${HOME}/.config/powershell"
+curl -Lo "${HOME}/.config/powershell/Microsoft.PowerShell_profile.ps1" https://raw.githubusercontent.com/cscribn/config-misc/main/powershell-core/Microsoft.PowerShell_profile.ps1
+
+# key bindings
+mkdir -p "${HOME}/Library/KeyBindings"
+
+echo '{
+/* Remap Home / End keys to be correct */
+"\UF729" = "moveToBeginningOfLine:"; /* Home */
+"\UF72B" = "moveToEndOfLine:"; /* End */
+"$\UF729" = "moveToBeginningOfLineAndModifySelection:"; /* Shift + Home */
+"$\UF72B" = "moveToEndOfLineAndModifySelection:"; /* Shift + End */
+"^\UF729" = "moveToBeginningOfDocument:"; /* Ctrl + Home */
+"^\UF72B" = "moveToEndOfDocument:"; /* Ctrl + End */
+"$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:"; /* Shift + Ctrl + Home */
+"$^\UF72B" = "moveToEndOfDocumentAndModifySelection:"; /* Shift + Ctrl + End */
+}' > "${HOME}/Library/KeyBindings/DefaultKeyBinding.dict"
