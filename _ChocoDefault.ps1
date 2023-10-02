2409d4e550c51cb13c44bbce1f7db9228ab28337
Chad Scribner <chadbscribner@gmail.com>
2023-10-02 14:52:25 -0500

    Adding linux-reader

--- _ChocoDefault.ps1
+++ _ChocoDefault.ps1
@@ -90,6 +90,7 @@ choco upgrade itunes -y
 choco upgrade kitty --version 0.76.1.9 -y
 choco pin add --name="'kitty'" --version="'0.76.1.9'"
 choco upgrade libreoffice-still -y
+choco upgrade linux-reader -y
 choco upgrade lsd -y
 choco upgrade microsoft-teams -y
 choco upgrade mp3tag -y
