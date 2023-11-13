ff7501a60e865380cf16a987e3379abf99c61ab4
Chad Scribner <chadbscribner@gmail.com>
2023-11-13 10:53:02 -0600

    Openbor support

--- apt-retropie.sh
+++ apt-retropie.sh
@@ -23,8 +23,9 @@ sudo apt install wondershaper -y
 sudo wondershaper eth0 10240 10240
 sudo wondershaper wlan0 10240 10240
 
-# RetroPie-Extra
+# retropie extras
 mkdir -p "/home/pi/RetroPie-Setup/scriptmodules/libretrocores"
+
 curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-duckstation.sh" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-duckstation.sh
 
 curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-swanstation.sh" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-swanstation.sh
@@ -33,6 +34,12 @@ curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro.sh
 
 mkdir -p "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro"
 curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro/01_shader_hack_rpi4.diff" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-yabasanshiro/01_shader_hack_rpi4.diff
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/ports/openbor-v6510-RPi0.sh" https://raw.githubusercontent.com/crcerror/OpenBOR-63xx-RetroPie-openbeta/master/scriptmodules/openbor-6xxx-RPizero/openbor-v6510-RPi0.sh
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/ports/openbor-v6510-RPi3.sh" https://raw.githubusercontent.com/crcerror/OpenBOR-63xx-RetroPie-openbeta/master/scriptmodules/openbor-6xxx-RPi3/openbor-v6510-RPi3.sh
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/ports/openbor-v6510-RPi4.sh" https://raw.githubusercontent.com/crcerror/OpenBOR-63xx-RetroPie-openbeta/master/scriptmodules/openbor-6xxx-RPi4/openbor-v6510-RPi4.sh
 
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
