ee9c85caf1e495159a1195cab38cd25eff091519
Chad Scribner <chadbscribner@gmail.com>
2023-12-15 17:10:23 -0600

    Creating fetch_remove function; adding motd support

--- _apt-default.sh
+++ _apt-default.sh
@@ -6,10 +6,27 @@
 declare script_name
 script_name=$(basename "${0}")
 declare script_dir
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-declare git_dir
-declare git_main
-declare git_origin
+script_dir="$(dirname "$0")"
+
+# helper functions
+fetch_remove() {
+	local git_dir="$1"
+	local branch="2"
+	local git_main
+	local git_origin
+
+	if [[ -d "$git_dir" ]]; then
+		cd "$git_dir" || exit
+		git fetch
+		git_main=$(git rev-parse ${branch})
+		git_origin=$(git rev-parse origin/${branch})
+		cd - || exit
+
+		if [[ "$git_main" != "$git_origin" ]]; then
+			rm -rf "$git_dir"
+		fi
+	fi
+}
 
 sudo apt update
 sudo apt install curl -y
@@ -44,33 +61,10 @@ sudo apt install vim -y
 
 # zsh
 sudo apt install zsh -y
+fetch_remove "${HOME}/.zsh/zsh-autosuggestions" "master"
+fetch_remove "${HOME}/.zsh/zsh-syntax-highlighting" "master"
 
-git_dir="${HOME}/.zsh/zsh-autosuggestions"
-
-if [[ -d "$git_dir" ]]; then
-	cd "$git_dir" || exit
-	git fetch
-	git_main=$(git rev-parse master)
-	git_origin=$(git rev-parse origin/master)
-	cd - || exit
-
-	if [[ "$git_main" != "$git_origin" ]]; then
-		rm -rf "$git_dir"
-	fi
-fi
-
-git_dir="${HOME}/.zsh/zsh-syntax-highlighting"
-
-if [[ -d "$git_dir" ]]; then
-	cd "$git_dir" || exit
-	git fetch
-	git_main=$(git rev-parse master)
-	git_origin=$(git rev-parse origin/master)
-	cd - || exit
-
-	if [[ "$git_main" != "$git_origin" ]]; then
-		rm -rf "$git_dir"
-	fi
-fi
+# motd
+fetch_remove "${HOME}/motd" "main"
 
 source "${script_dir}/_apt-default-config.sh"
