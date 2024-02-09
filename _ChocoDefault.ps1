09a59b2f67ac28834444e955a7026f61673f7849
Chad Scribner <chadbscribner@gmail.com>
2024-02-09 10:58:25 -0600

    Adding netflix

--- _ChocoDefault.ps1
+++ _ChocoDefault.ps1
@@ -98,8 +98,6 @@ choco upgrade imgburn -y
 choco upgrade inkscape -y
 choco upgrade irfanview -y
 choco upgrade irfanviewplugins -y
-choco uninstall itunes -y
-choco pin remove -n=kitty
 choco upgrade kitty -y
 choco upgrade libreoffice-still -y
 choco upgrade linux-reader -y
