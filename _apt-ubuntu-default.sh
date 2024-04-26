d251acca88b7e5df45532536affbdae14008f85e
Chad Scribner <chadbscribner@gmail.com>
2024-04-26 10:33:00 -0500

    Adding ubuntu scripts

--- _apt-ubuntu-default.sh
+++ _apt-ubuntu-default.sh
@@ -34,7 +34,7 @@ fetch_remove() {
 sudo apt update
 sudo apt autoremove -y
 sudo apt clean -y
-sudo apt dist-upgrade -y
+# sudo apt dist-upgrade -y
 
 sudo apt install curl -y
 sudo apt install git -y
@@ -42,7 +42,7 @@ rustup update
 cargo install lsd
 
 # motd
-fetch_remove "${HOME}/motd" "main"
+# fetch_remove "${HOME}/motd" "main"
 
 # oh-my-posh
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-arm -O /usr/local/bin/oh-my-posh
@@ -53,19 +53,19 @@ sudo chmod +x /usr/local/bin/oh-my-posh
 rm -rf "${HOME}/powershell"
 mkdir "${HOME}/powershell"
 curl -s https://api.github.com/repos/PowerShell/PowerShell/releases/latest \
-	| grep "https://github.com/PowerShell/PowerShell/releases/download/.*/powershell-.*-linux-arm32.tar.gz" \
+	| grep "https://github.com/PowerShell/PowerShell/releases/download/.*/powershell-.*-linux-arm64.tar.gz" \
 	| cut -d : -f 2,3 | tr -d \" | wget -qi -
-tar -xvf ./powershell-*-linux-arm32.tar.gz -C "${HOME}/powershell"
-rm -f ./powershell-*-linux-arm32.tar.gz
+tar -xvf ./powershell-*-linux-arm64.tar.gz -C "${HOME}/powershell"
+rm -f ./powershell-*-linux-arm64.tar.gz
 "${HOME}/powershell/pwsh" ./PwshLinux.ps1
 
 sudo apt install python-pip -y
 sudo apt install python3-pip -y
 
 # raspi2png
-git clone https://github.com/AndrewFromMelbourne/raspi2png
-sudo cp -a raspi2png/raspi2png /usr/local/bin
-rm -rf ./raspi2png
+# git clone https://github.com/AndrewFromMelbourne/raspi2png
+# sudo cp -a raspi2png/raspi2png /usr/local/bin
+# rm -rf ./raspi2png
 
 # ruby
 sudo apt install rbenv -y
@@ -96,4 +96,4 @@ sudo apt install zsh -y
 fetch_remove "${HOME}/.zsh/zsh-autosuggestions" "master"
 fetch_remove "${HOME}/.zsh/zsh-syntax-highlighting" "master"
 
-source "${script_dir}/_apt-pi-default-config.sh"
+source "${script_dir}/_apt-ubuntu-default-config.sh"
---
+++ apt-ark.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# settings
+set -o nounset
+set -o pipefail
+[[ "${TRACE-0}" = "1" ]] && set -o xtrace
+
+# variables
+declare script_name
+script_name=$(basename "${0}")
+
+# usage
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+	echo "Usage: ./${script_name}"
+	exit
+fi
+
+# include
+source ./_apt-pi-default.sh
+
+# wondershaper
+sudo apt install wondershaper -y
+sudo wondershaper eth0 10240 10240
+sudo wondershaper wlan0 10240 10240
+
+# retropie extras
+mkdir -p "/home/pi/RetroPie-Setup/scriptmodules/libretrocores"
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-duckstation.sh" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-duckstation.sh
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-swanstation.sh" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-swanstation.sh
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro.sh" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-yabasanshiro.sh
+
+mkdir -p "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro"
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-yabasanshiro/01_shader_hack_rpi4.diff" https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/libretrocores/lr-yabasanshiro/01_shader_hack_rpi4.diff
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/ports/openbor-v6510-RPi0.sh" https://raw.githubusercontent.com/crcerror/OpenBOR-63xx-RetroPie-openbeta/master/scriptmodules/openbor-6xxx-RPizero/openbor-v6510-RPi0.sh
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/ports/openbor-v6510-RPi3.sh" https://raw.githubusercontent.com/crcerror/OpenBOR-63xx-RetroPie-openbeta/master/scriptmodules/openbor-6xxx-RPi3/openbor-v6510-RPi3.sh
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/ports/openbor-v6510-RPi4.sh" https://raw.githubusercontent.com/crcerror/OpenBOR-63xx-RetroPie-openbeta/master/scriptmodules/openbor-6xxx-RPi4/openbor-v6510-RPi4.sh
+
+curl -Lo "/home/pi/RetroPie-Setup/scriptmodules/supplementary/mamedev.sh" https://raw.githubusercontent.com/FollyMaddy/RetroPie-Share/main/00-scriptmodules-00/supplementary/mamedev.sh
+
+# .zshrc1
+cp "${HOME}/.config/zsh/zshrc1-retropie" "${HOME}/.zshrc1"
