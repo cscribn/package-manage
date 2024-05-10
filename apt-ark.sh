40e11c2823210d405dcde4ab12d6fa40750dba85
Chad Scribner <chadbscribner@gmail.com>
2024-05-10 15:39:23 -0500

    Adding linking of singe directory

--- apt-ark.sh
+++ apt-ark.sh
@@ -18,5 +18,9 @@ fi
 # include
 source ./_apt-ubuntu-default.sh
 
+# properly link singe directory
+unlink /opt/hypseus-singe/singe
+ln -sfv /roms/daphne/roms /opt/hypseus-singe/singe
+
 # enable ssh
 sudo systemctl enable ssh
