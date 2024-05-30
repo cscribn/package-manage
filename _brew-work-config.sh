468c2363a78fbc5156825b26a4635d1824239c8d
Chad Scribner <chadbscribner@gmail.com>
2024-05-30 15:56:19 -0500

    More converting to one-liners

--- _brew-work-config.sh
+++ _brew-work-config.sh
@@ -1,73 +1,24 @@
 #!/usr/bin/env bash
 
-# variables
-declare clone
-declare git_dir
-declare git_main
-declare git_origin
-declare git_url
-
 # bash
 curl -Lo "${HOME}/.bashrc" https://raw.githubusercontent.com/cscribn/config-misc/main/bash/bashrc-mac
 
 # bbedit
-curl -Lo "${HOME}/Library/Application Support/BBEdit/Language Modules/CSharpLanguageModule.plist" https://luminaryapps.com/code/CSharpLanguageModule.plist
+curl -Lo "${HOME}/Library/Application Support/BBEdit/Language Modules/CSharpLanguageModule.plist" https://luminaryapps.com/code/CSharpLanguageModule.plist; \
 curl -Lo "${HOME}/Library/Application Support/BBEdit/Language Modules/PowerShell.plist" https://raw.githubusercontent.com/doug-baer/BBEdit-PowerShell/master/PowerShell.plist
 
 # oh-my-posh
-git_dir="${HOME}/.config/oh-my-posh"
-git_url="https://github.com/cscribn/config-oh-my-posh.git"
-clone=0
-
-if [[ ! -d "$git_dir" ]]; then
-	clone=1
-else
-	cd "$git_dir" || exit
-	git fetch
-	git_main=$(git rev-parse main)
-	git_origin=$(git rev-parse origin/main)
-	cd - || exit
-
-	if [[ "$git_main" != "$git_origin" ]]; then
-		rm -rf "$git_dir"
-		clone=1
-	fi
-fi
-
-if [[ "$clone" = 1 ]]; then
-	git clone "$git_url" "$git_dir"
-fi
+git_dir="${HOME}/.config/oh-my-posh"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull; cd -; else git clone "https://github.com/cscribn/config-oh-my-posh.git" "$git_dir"; fi
 
 # vim
 curl -Lo "${HOME}/.vimrc" https://raw.githubusercontent.com/cscribn/config-misc/main/vim/vimrc
 
 # zsh
-git_dir="${HOME}/.config/zsh"
-git_url="https://github.com/cscribn/config-zsh.git"
-clone=0
-
-if [[ ! -d "$git_dir" ]]; then
-	clone=1
-else
-	cd "$git_dir" || exit
-	git fetch
-	git_main=$(git rev-parse main)
-	git_origin=$(git rev-parse origin/main)
-	cd - || exit
-
-	if [[ "$git_main" != "$git_origin" ]]; then
-		rm -rf "$git_dir"
-		clone=1
-	fi
-fi
-
-if [[ "$clone" = 1 ]]; then
-	git clone "$git_url" "$git_dir"
-	cp ~/.config/zsh/zshrc-mac ~/.zshrc
-fi
+git_dir="${HOME}/.config/zsh"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull; cd -; else git clone "https://github.com/cscribn/config-zsh.git" "$git_dir"; fi; \
+cp "${HOME}/.config/zsh/zshrc-mac" "${HOME}/.zshrc"
 
 # pwsh
-mkdir -p "${HOME}/.config/powershell"
+mkdir -p "${HOME}/.config/powershell"; \
 curl -Lo "${HOME}/.config/powershell/Microsoft.PowerShell_profile.ps1" https://raw.githubusercontent.com/cscribn/config-misc/main/powershell-core/Microsoft.PowerShell_profile.ps1
 
 # key bindings
--- brew-work.sh
+++ brew-work.sh
@@ -6,47 +6,6 @@
 # sudo git lfs install --system
 # paste plain text
 
-# settings
-set -o nounset
-set -o pipefail
-[[ "${TRACE-0}" = "1" ]] && set -o xtrace
-
-# variables
-declare script_name
-script_name=$(basename "${0}")
-declare script_dir
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-declare git_dir
-declare git_main
-declare git_origin
-declare git_url
-
-# helper functions
-fetch_remove() {
-	local git_dir="$1"
-	local branch="$2"
-	local git_main
-	local git_origin
-
-	if [[ -d "$git_dir" ]]; then
-		cd "$git_dir" || exit
-		git fetch
-		git_main=$(git rev-parse ${branch})
-		git_origin=$(git rev-parse origin/${branch})
-		cd - || exit
-
-		if [[ "$git_main" != "$git_origin" ]]; then
-			rm -rf "$git_dir"
-		fi
-	fi
-}
-
-# usage
-if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-	echo "Usage: ./${script_name}"
-	exit
-fi
-
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}"
 
 brew update || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -74,11 +33,7 @@ brew install --cask docker || brew upgrade --cask docker
 brew install ffmpeg || brew upgrade ffmpeg
 brew install --cask firefox || install --cask firefox
 brew install --cask gimp || brew upgrade --cask gimp
-
-# git-lfs
-brew install git-lfs || brew upgrade git-lfs
-git lfs install
-
+brew install git-lfs || brew upgrade git-lfs; git lfs install
 brew install --cask font-meslo-lg-nerd-font || brew upgrade --cask font-meslo-lg-nerd-font
 brew install gifsicle || brew upgrade gifsicle
 brew install git || brew upgrade git
@@ -92,34 +47,24 @@ brew install htop || brew upgrade htop
 brew install --cask iterm2 || brew upgrade --cask iterm2
 brew install --cask itsycal ||brew upgrade --cask itsycal
 brew install jq ||brew upgrade jq
-
-# psql
-brew install libpq || brew upgrade libpq
-brew link --force libpq
-
+brew install libpq || brew upgrade libpq;brew link --force libpq
 brew install --cask libreoffice-still || brew upgrade --cask libreoffice-still
 brew install lsd || brew upgrade lsd
 brew install --cask microsoft-edge || brew upgrade --cask microsoft-edge
 brew install --cask microsoft-auto-update || brew upgrade --cask microsoft-auto-update
 
 # node
-brew install nvm || brew upgrade nvm
-export NVM_DIR="${HOME}/.nvm"
-[[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-nvm install --lts
-nvm use --lts
+brew install nvm || brew upgrade nvm; export NVM_DIR="${HOME}/.nvm"; \
+[[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && \. "/opt/homebrew/opt/nvm/nvm.sh"; \
+[[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"; \
+nvm install --lts; nvm use --lts;
 
 # remove old node versions
-cd "${HOME}/.nvm/versions/node" || exit
-\ls -dr * | tail -n +2 | xargs -I '{}' bash -c "export NVM_DIR=$HOME/.nvm; [ -s $NVM_DIR/nvm.sh ] && \. $NVM_DIR/nvm.sh; nvm deactivate {} && nvm uninstall {}"
+cd "${HOME}/.nvm/versions/node" || exit; \
+\ls -dr * | tail -n +2 | xargs -I '{}' bash -c "export NVM_DIR=$HOME/.nvm; [ -s $NVM_DIR/nvm.sh ] && \. $NVM_DIR/nvm.sh; nvm deactivate {} && nvm uninstall {}"; \
 cd - || exit
 
-# oh-my-posh
-brew install jandedobbeleer/oh-my-posh/oh-my-posh || brew upgrade jandedobbeleer/oh-my-posh/oh-my-posh
-oh-my-posh disable notice
-
+brew install jandedobbeleer/oh-my-posh/oh-my-posh || brew upgrade jandedobbeleer/oh-my-posh/oh-my-posh; oh-my-posh disable notice
 brew install --cask oracle-jdk@17 || brew upgrade --cask oracle-jdk@17
 brew install perl || brew upgrade perl
 brew install --cask pinta || brew upgrade --cask pinta
@@ -131,9 +76,8 @@ brew install pre-commit || brew upgrade pre-commit
 brew install --cask rectangle || brew upgrade --cask rectangle
 
 # ruby
-brew install rbenv || brew upgrade rbenv
-rm -rf "${HOME}/.rbenv/plugins/ruby-build"
-git clone https://github.com/rbenv/ruby-build.git "${HOME}/.rbenv/plugins/ruby-build"
+brew install rbenv || brew upgrade rbenv; rm -rf "${HOME}/.rbenv/plugins/ruby-build"; \
+git clone "https://github.com/rbenv/ruby-build.git" "${HOME}/.rbenv/plugins/ruby-build"
 
 brew install --cask the-unarchiver || brew upgrade --cask the-unarchiver
 brew install tsh || brew upgrade tsh
@@ -144,7 +88,9 @@ brew install yarn || brew upgrade yarn
 
 # zsh
 brew install zsh || brew upgrade zsh
-fetch_remove "${HOME}/.zsh/zsh-autosuggestions" "master"
-fetch_remove "${HOME}/.zsh/zsh-syntax-highlighting" "master"
 
+git_dir="${HOME}/.zsh/zsh-autosuggestions"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull; cd -; else git clone "https://github.com/zsh-users/zsh-autosuggestions" "$git_dir"; fi
+git_dir="${HOME}/.zsh/zsh-syntax-highlighting"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull; cd -; else git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git"; fi
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ); \
 source "${script_dir}/_brew-work-config.sh"
