02902613dff37fec01c81ee76b4004c17e91a079
Chad Scribner <chadbscribner@gmail.com>
2024-08-16 09:56:34 -0500

    Refactoring to use one-liners

--- _ubuntu-default-config.sh
+++ _ubuntu-default-config.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
 
-# variables
-declare clone
-
-# helper functions
-fetch_clone() {
-	local git_dir="$1"
-	local git_url="$2"
-	local branch="$3"
-	local git_main
-	local git_origin
-	clone=0
-
-	if [[ ! -d "$git_dir" ]]; then
-		clone=1
-	else
-		cd "$git_dir" || exit
-		git fetch
-		git_main=$(git rev-parse $branch)
-		git_origin=$(git rev-parse origin/$branch)
-		cd - || exit
-
-		if [[ "$git_main" != "$git_origin" ]]; then
-			rm -rf "$git_dir"
-			clone=1
-		fi
-	fi
-
-	if [[ "$clone" = 1 ]]; then
-		git clone "$git_url" "$git_dir"
-	fi
-}
-
 # oh-my-posh
-fetch_clone "${HOME}/.config/oh-my-posh" "https://github.com/cscribn/dotfiles-oh-my-posh.git" "main"
+git_dir="${HOME}/.config/oh-my-posh"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull; cd -; else git clone "https://github.com/cscribn/dotfiles-oh-my-posh.git" "$git_dir"; fi
 
 # pwsh
 mkdir -p "${HOME}/.config/powershell"; \
@@ -43,5 +11,5 @@ curl -Lo "${HOME}/.config/powershell/Microsoft.PowerShell_profile.ps1" https://r
 curl -Lo "${HOME}/.vimrc" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/vim/vimrc
 
 # zsh
-fetch_clone "${HOME}/.config/zsh" "https://github.com/cscribn/dotfiles-zsh.git" "main"
-[[ "$clone" = 1 ]] && cp "${HOME}/.config/zsh/zshrc-ubuntu" "${HOME}/.zshrc"
+git_dir="${HOME}/.config/zsh"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull; cd -; else git clone "https://github.com/cscribn/dotfiles-zsh.git" "$git_dir"; fi; \
+cp "${HOME}/.config/zsh/zshrc-ubuntu" "${HOME}/.zshrc"
