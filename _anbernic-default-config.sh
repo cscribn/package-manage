bdad1c5b641a0f97a667e95f7cefd1d19a95d72e
Chad Scribner <chadbscribner@gmail.com>
2024-09-10 19:49:39 -0500

    Fixing dir remove

--- _anbernic-default-config.sh
+++ _anbernic-default-config.sh
@@ -7,7 +7,7 @@ curl -Lo "${HOME}/.bashrc" https://raw.githubusercontent.com/cscribn/dotfiles-mi
 config_dir="${HOME}/.config/oh-my-posh"; \
 repo_url="https://github.com/cscribn/dotfiles-oh-my-posh/archive/refs/heads/main.zip"; \
 temp_zip="${HOME}/oh-my-posh.zip"; \
-[[ -d "$config_dir" ]] && rm -rf "${config_dir:?}"/*; \
+[[ -d "$config_dir" ]] && rm -rf "$config_dir"; \
 wget -O "$temp_zip" "$repo_url"; \
 unzip -o "$temp_zip" -d "$HOME"; \
 mv "${HOME}/dotfiles-oh-my-posh-main" "$config_dir"; \
