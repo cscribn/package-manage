7e0cf30d2dc0805e7739698f988f208dd940eef0
Chad Scribner <chadbscribner@gmail.com>
2024-09-11 09:31:41 -0500

    Revert "Fixing paths"
    This reverts commit e9d3e9f6e959253b3a838f8321c0779e75557a93.

--- _anbernic-default-config.sh
+++ _anbernic-default-config.sh
@@ -6,12 +6,13 @@ curl -Lo "${HOME}/.bashrc" https://raw.githubusercontent.com/cscribn/dotfiles-mi
 # oh-my-posh
 config_dir="${HOME}/.config/oh-my-posh"; \
 repo_url="https://github.com/cscribn/dotfiles-oh-my-posh/archive/refs/heads/main.zip"; \
-temp_zip="${HOME}/oh-my-posh.zip"; \
+temp_zip="/tmp/oh-my-posh.zip"; \
 [[ -d "$config_dir" ]] && rm -rf "${config_dir:?}"/*; \
+mkdir -p "$config_dir"; \
 wget -O "$temp_zip" "$repo_url"; \
-unzip -o "$temp_zip" -d "$HOME"; \
-mv "${HOME}/dotfiles-oh-my-posh-main" "$config_dir"; \
-rm -f "$temp_zip"
+unzip -o "$temp_zip" -d /tmp/; \
+mv /tmp/dotfiles-oh-my-posh-main/*; \
+rm -rf /tmp/dotfiles-oh-my-posh-main/ "$temp_zip"
 
 # vim
 curl -Lo "${HOME}/.vimrc" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/vim/vimrc
