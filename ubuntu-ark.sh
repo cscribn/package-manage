a4a6c48422d557b8b502fa6a8d18507e3ab2d645
Chad Scribner <chadbscribner@gmail.com>
2024-09-16 14:06:51 -0500

    Adding ark motd. Fixing space.

--- ubuntu-ark.sh
+++ ubuntu-ark.sh
@@ -4,7 +4,10 @@
 source ./_ubuntu-default.sh
 
 # properly link singe directory
-unlink /opt/hypseus-singe/singe;ln -sfv /roms/daphne/roms /opt/hypseus-singe/singe
+unlink /opt/hypseus-singe/singe; ln -sfv /roms/daphne/roms /opt/hypseus-singe/singe
 
 # enable ssh
 sudo systemctl enable ssh
+
+# .zshrc1
+cp "${HOME}/.config/zsh/zshrc1-ark" "${HOME}/.zshrc1"
