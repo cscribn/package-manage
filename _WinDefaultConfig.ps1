db383d8d24ec52aa95518537dbb338c10b58b6db
Chad Scribner <chadbscribner@gmail.com>
2024-09-18 11:48:08 -0500

    Adding microsoft-windows-terminal profile icons

--- _WinDefaultConfig.ps1
+++ _WinDefaultConfig.ps1
@@ -4,6 +4,12 @@ curl -Lo "$Env:USERPROFILE\.bashrc" https://raw.githubusercontent.com/cscribn/do
 # clink
 curl -Lo "$Env:LOCALAPPDATA\clink\oh-my-posh.lua" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/clink/oh-my-posh.lua
 
+# microsoft-windows-terminal
+New-Item -ItemType Directory -Force -Path "$Env:USERPROFILE\.config\microsoft-windows-terminal\ProfileIcons"; `
+curl -Lo "$Env:USERPROFILE\.config\microsoft-windows-terminal\ProfileIcons\git-for-windows.ico" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/microsoft-windows-terminal/ProfileIcons/git-for-windows.ico; `
+curl -Lo "$Env:USERPROFILE\.config\microsoft-windows-terminal\ProfileIcons\ssh.ico" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/microsoft-windows-terminal/ProfileIcons/ssh.ico; `
+curl -Lo "$Env:USERPROFILE\.config\microsoft-windows-terminal\ProfileIcons\zsh.ico" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/microsoft-windows-terminal/ProfileIcons/zsh.ico
+
 # oh-my-posh
 $GitDir = "$Env:USERPROFILE\.config\oh-my-posh"; If (Test-Path $GitDir) { Set-Location $GitDir; git pull; Set-Location - } Else { git clone "https://github.com/cscribn/dotfiles-oh-my-posh.git" $GitDir}
 
@@ -16,4 +22,3 @@ Set-Location "$Env:USERPROFILE"; curl -Lo ".vimrc" https://raw.githubusercontent
 # zsh
 $GitDir = "$Env:USERPROFILE\.config\zsh"; If (Test-Path $GitDir) { Set-Location $GitDir; git pull; Set-Location - } Else { git clone "https://github.com/cscribn/dotfiles-zsh.git" $GitDir}; `
 Copy-Item -Force -Path "$GitDir\zshrc-win" -Destination "$Env:USERPROFILE\.zshrc"; `
-Copy-Item -Recurse -Force -Path "$GitDir\zsh.ico" -Destination "C:\Program Files\Git\usr\share\icons\locolor\32x32\apps"
