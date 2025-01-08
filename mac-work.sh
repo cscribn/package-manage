9b3908603f5e14f2427873fe7143579b6c1cfef5
Chad Scribner <chad-scribner@pluralsight.com>
2025-01-08 14:23:47 -0600

    Delete rectangle, now supported by macos

--- mac-work.sh
+++ mac-work.sh
@@ -76,7 +76,6 @@ brew install --cask postgres-unofficial || brew upgrade --cask postgres-unoffici
 brew install --cask postman || brew upgrade --force --cask postman
 brew install --cask powershell || brew upgrade --cask powershell
 brew install pre-commit || brew upgrade pre-commit
-brew install --cask rectangle || brew upgrade --cask rectangle
 sdk selfupdate || curl -s "https://get.sdkman.io" | bash
 brew install snyk || brew upgrade snyk
 brew install --cask the-unarchiver || brew upgrade --cask the-unarchiver
