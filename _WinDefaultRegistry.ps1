2ddbafc1e9bead56ca41d115b33e08cfad645b9c
Chad Scribner <chadbscribner@gmail.com>
2025-01-30 20:34:39 -0600

    Add seconds to clock

--- _WinDefaultRegistry.ps1
+++ _WinDefaultRegistry.ps1
@@ -1,3 +1,6 @@
+# Clock - show seconds
+[Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "ShowSecondsInSystemClock", 1)
+
 # Data Collection - disable telemetry
 [Microsoft.Win32.Registry]::SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection", "AllowTelemetry", 0)
 
