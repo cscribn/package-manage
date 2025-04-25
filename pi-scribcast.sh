7746647a4bf079e8579b4ce0193a1530384cdd24
Chad Scribner <chadbscribner@gmail.com>
2025-04-25 09:04:32 -0500

    Minor updates for plexamp install

--- pi-scribcast.sh
+++ pi-scribcast.sh
@@ -6,7 +6,10 @@
 source ./_pi-default.sh
 
 # plexamp - https://gist.github.com/tgp-2/fc34c5389bc3e4ef332e28d9430b0ebf
-wget https://gist.githubusercontent.com/tgp-2/65e6f2f637bc81df2c9fd9ba33f73bc6/raw/e7e9e47046c29a6090042a9a0a868a5bf7cf48be/plexamp-install.sh
+wget -O "${HOME}/plexamp-install.sh" https://gist.githubusercontent.com/tgp-2/65e6f2f637bc81df2c9fd9ba33f73bc6/raw/e7e9e47046c29a6090042a9a0a868a5bf7cf48be/plexamp-install.sh
 
 # autoremove, clean
 sudo apt autoremove -y; sudo apt clean -y
+
+echo "Copy and paste the following to upgrade plexamp:"
+echo "cd ${HOME} && bash ./plexamp-install.sh"
