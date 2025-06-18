07b303927f2841d25e9d4af73f58f137fd0ba0ad
Chad Scribner <chad.scribner@appfire.com>
2025-06-18 11:27:22 -0500

    Another password fix

--- mac-work.sh
+++ mac-work.sh
@@ -10,7 +10,9 @@
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}"
 
 # Check password before proceeding
-if ! dscl /Search -authonly "$USERNAME" "$SUDO_ASKPASS" &>/dev/null; then
+if dscl /Search -authonly "$USERNAME" "$SUDO_ASKPASS"; then
+    echo "Password is correct."
+else
     echo "Password is incorrect."
     exit 1
 fi
