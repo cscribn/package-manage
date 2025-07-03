5a49e503b691e18f3880e74d9dd507b813312437
Chad Scribner <chadbscribner@gmail.com>
2025-07-03 15:38:56 -0500

    Adding db browser for sqlite

--- WinMedia.ps1
+++ WinMedia.ps1
@@ -5,6 +5,7 @@
 . $PSScriptRoot\_WinDefaultFirst.ps1
 
 winget install -e --id DBeaver.DBeaver.Community
+winget install -e --id DBBrowserForSQLite.DBBrowserForSQLite
 winget install -e --id Dell.CommandUpdate.Universal
 winget install -e --id Google.GoogleDrive
 winget install -e --id NextDNS.NextDNS
