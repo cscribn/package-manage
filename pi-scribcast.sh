d10efc3b2068d46f32de0c0edd5986626e5a2aac
Chad Scribner <chadbscribner@gmail.com>
2025-07-13 15:29:53 -0500

    Updating volume and giving headphones direction

--- pi-scribcast.sh
+++ pi-scribcast.sh
@@ -8,8 +8,12 @@ source ./_pi-default.sh
 # plexamp - https://gist.github.com/tgp-2/fc34c5389bc3e4ef332e28d9430b0ebf
 wget -O "${HOME}/plexamp-install.sh" https://gist.githubusercontent.com/tgp-2/65e6f2f637bc81df2c9fd9ba33f73bc6/raw/e7e9e47046c29a6090042a9a0a868a5bf7cf48be/plexamp-install.sh
 
+# set volume to 100%
+amixer set PCM -- 100%
+
 # autoremove, clean
 sudo apt autoremove -y; sudo apt clean -y
 
 echo "Copy and paste the following to upgrade plexamp:"
 echo "cd ${HOME} && bash ./plexamp-install.sh"
+echo "After installation, make sure Settings -> Playback -> Audio Output -> Audio Device is set to headphones."
