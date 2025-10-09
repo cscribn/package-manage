d4b7e05536f6f8ca58937792ab2e14b477dd5a4c
Chad Scribner <chadbscribner@gmail.com>
2025-10-09 07:14:18 -0500

    Disable browsing history in start menu.

--- _WinDefaultRegistry.ps1
+++ _WinDefaultRegistry.ps1
@@ -108,6 +108,9 @@ Remove-ItemProperty –Path "HKCU:\SOFTWARE\Classes\AppX6eg8h5sxqq90pv53845wmnbe
 # Start Menu - disable Internet search
 [Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer", "DisableSearchBoxSuggestions", 1)
 
+# Start Menu - disable browsing history
+[Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\SearchSettings", "IsDynamicSearchBoxEnabled", 0)
+
 # Start Menu - disable show mobile device
 [Microsoft.Win32.Registry]::SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Start\Companions\Microsoft.YourPhone_8wekyb3d8bbwe", "IsEnabled", 0)
 
