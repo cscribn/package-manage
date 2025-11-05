c9f27ae1900b4c1a5d08ee4b61d4efb9443d8a7b
Chad Scribner <chadbscribner@gmail.com>
2025-11-05 08:43:16 -0600

    Make lighter.

--- _WinDefaultNetwork.ps1
+++ _WinDefaultNetwork.ps1
@@ -4,21 +4,8 @@ Set-NetFirewallProfile -Profile Private -Enabled False
 # Flush the DNS cache
 ipconfig /flushdns
 
-# Release and renew the DHCP lease
-ipconfig /release; `
-ipconfig /renew
+# Clears NetBIOS cache
+nbtstat -R
 
-# Clear and rebuild the ARP cache
-arp -d *
-
-# Restart Workstation and Server services (these handle SMB connections)
-net stop workstation /y; `
-net start workstation; `
-net start sessionenv; `
-net stop server /y; `
-net start server
-
-# Restart Docker Desktop Service (if it exists)
-if (Get-Service -Name "com.docker.service" -ErrorAction SilentlyContinue) { `
-    Restart-Service -Name "com.docker.service" -Force `
-}
+# Refreshes NetBIOS registration
+nbtstat -RR
