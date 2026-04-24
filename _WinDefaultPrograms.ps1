4d9ba7e31df46d4a3226b50f4f94bbcc3eaabe9e
Chad Scribner <chadbscribner@gmail.com>
2026-04-23 20:27:06 -0500

    Add video editors.

--- _WinDefaultPrograms.ps1
+++ _WinDefaultPrograms.ps1
@@ -96,10 +96,12 @@ winget install -e --id Mythicsoft.AgentRansack
 winget install -e --id Notepad++.Notepad++
 winget install -e --id OPAutoClicker.OPAutoClicker
 winget install -e --id JohnMacFarlane.Pandoc
+winget install -e --id KDE.Kdenlive
 winget install -e --id PDFLabs.PDFtk.Free
 winget install -e --id PFOJEnterprisesLLC.ModernCSV
 winget install -e --id RandyRants.SharpKeys
 winget install -e --id Scribus.Scribus
+winget install -e --id Meltytech.Shotcut
 winget install -e --id SumatraPDF.SumatraPDF
 winget install -e --id TGRMNSoftware.BulkRenameUtility
 winget install -e --id TheDocumentFoundation.LibreOffice
