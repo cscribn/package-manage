49a1a9ee58fd1072eef8d94a83c602959da4afb1
Chad Scribner <chadbscribner@gmail.com>
2026-05-17 20:07:54 -0500

    Change gradle to upgrade.

--- WinChad.ps1
+++ WinChad.ps1
@@ -8,8 +8,8 @@
 . $PSScriptRoot\_WinDefaultPrograms.ps1
 
 choco upgrade fluidsynth -y --ignore-dependencies
-choco install gradle -y --ignore-dependencies
 choco upgrade iconsext -y --ignore-dependencies; choco upgrade iconsext.install -y --ignore-dependencies
+choco upgrade gradle -y --ignore-dependencies
 choco upgrade pngquant -y --ignore-dependencies
 choco upgrade pngyu -y --ignore-dependencies
 choco upgrade tftpd32 -y --ignore-dependencies
