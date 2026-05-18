0049b657e47c781e41afd1bb4e13a658783f5c35
Chad Scribner <chadbscribner@gmail.com>
2026-05-17 19:33:33 -0500

    Remove modern csv.

--- _WinDefaultPrograms.ps1
+++ _WinDefaultPrograms.ps1
@@ -100,7 +100,7 @@ winget install -e --id Notepad++.Notepad++
 winget install -e --id OPAutoClicker.OPAutoClicker
 winget install -e --id JohnMacFarlane.Pandoc
 winget install -e --id PDFLabs.PDFtk.Free
-winget install -e --id PFOJEnterprisesLLC.ModernCSV
+winget uninstall -e --id PFOJEnterprisesLLC.ModernCSV
 winget install -e --id RandyRants.SharpKeys
 winget install -e --id Scribus.Scribus
 winget install -e --id Meltytech.Shotcut
