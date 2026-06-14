19a3dfacd0ddf095c13df3e6d41a62f366310fe8
Chad Scribner <chadbscribner@gmail.com>
2026-06-14 13:16:05 -0500

    Update copilot, caveman, rtk.

--- WinChad.ps1
+++ WinChad.ps1
@@ -9,8 +9,8 @@
 . $PSScriptRoot\_WinDefaultPrograms.ps1
 
 # copilot instructions - copy early for appending later
-New-Item -ItemType Directory -Force -Path "$Env:USERPROFILE\.copilot"; `
-curl -Lo "$Env:USERPROFILE\.copilot\copilot-instructions.md" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/github/copilot-instructions.md
+New-Item -ItemType Directory -Force -Path "$Env:APPDATA\Roaming\Code\User\prompts";
+curl -Lo "$Env:APPDATA\Roaming\Code\User\prompts\copilot-instructions.md" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/github/copilot-instructions.md
 
 choco upgrade fluidsynth -y --ignore-dependencies
 choco upgrade gradle -y --ignore-dependencies
@@ -25,8 +25,8 @@ winget install -e --id sharkdp.bat
 winget install -e --id BlueStack.BlueStacks
 
 # caveman ai
-"`n## Caveman AI`n" | Add-Content -Path "$env:USERPROFILE\.copilot\copilot-instructions.md"; `
-(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/JuliusBrussee/caveman/main/src/rules/caveman-activate.md" -UseBasicParsing).Content | Add-Content -Path "$env:USERPROFILE\.copilot\copilot-instructions.md"
+"`n## Caveman AI`n" | Add-Content -Path "$Env:APPDATA\Roaming\Code\User\prompts\copilot-instructions.md"; `
+(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/JuliusBrussee/caveman/main/src/rules/caveman-activate.md" -UseBasicParsing).Content | Add-Content -Path "$Env:APPDATA\Roaming\Code\User\prompts\copilot-instructions.md"
 
 winget install -e --id DBBrowserForSQLite.DBBrowserForSQLite
 
@@ -86,7 +86,7 @@ if ((Get-WinGetPackage Python.Python).Count -gt 1) { `
 }
 
 winget install -e --id RaspberryPiFoundation.RaspberryPiImager
-winget install -e --id rtk-ai.rtk; rtk init --global --copilot
+winget install -e --id rtk-ai.rtk;
 winget install -e --id Rufus.Rufus
 winget install -e --id SBCL.SBCL # steel bank common lisp
 winget install -e --id srjuddington.slade
