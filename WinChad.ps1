f39c970667eae2b8c597dbe81e4820bc7b544d38
Chad Scribner <chadbscribner@gmail.com>
2026-07-15 20:28:32 -0500

    Add yq.

--- WinChad.ps1
+++ WinChad.ps1
@@ -101,6 +101,7 @@ wsl -d "Ubuntu" -u root -e do-release-upgrade; `
 wsl -d "Ubuntu" -u root -e apt autoremove -y; `
 wsl -d "Ubuntu" -u root -e apt clean -y
 
+winget install -e --id MikeFarah.yq
 pip install "yt-dlp[default]"
 
 # config
