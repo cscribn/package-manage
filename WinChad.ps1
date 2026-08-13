4e06f1b7f9440ba0603f6c505492a60279dc0f84
Chad Scribner <chadbscribner@gmail.com>
2026-08-13 13:33:25 -0500

    Update to use wingetutils.

--- WinChad.ps1
+++ WinChad.ps1
@@ -7,6 +7,11 @@
 # mise (first install only from https://github.com/jdx/mise/releases)
 # skraperui
 
+[CmdletBinding()]
+$Global:InformationPreference = 'Continue'
+
+Import-Module "$PSScriptRoot\WingetUtils.psm1" -Force
+
 Get-Date -Format "dddd, MMMM dd, yyyy - hh:mm tt"
 . $PSScriptRoot\_WinDefaultPrograms.ps1
 
@@ -23,35 +28,26 @@ choco upgrade win32diskimager --version 0.9.5 -y --ignore-dependencies; choco pi
 choco upgrade xmlstarlet -y --ignore-dependencies; choco upgrade xmlstarlet.portable -y --ignore-dependencies
 choco upgrade xsltproc -y --ignore-dependencies
 
-winget install -e --id sharkdp.bat
+Install-WinGetPackageClean -Id sharkdp.bat
 
 # bats-core
 git clone https://github.com/bats-core/bats-core.git; Set-Location bats-core; bash ./install.sh "$HOME"; Set-Location -; `
 Remove-Item -Recurse -Force bats-core
 
-winget install -e --id BlueStack.BlueStacks
-winget install -e --id DBBrowserForSQLite.DBBrowserForSQLite
-
-if ((Get-WinGetPackage -Name "DiskGenius").Count -eq 0) { `
-    winget install -e --id Eassos.DiskGenius `
-}
-
-winget install -e --id Deskflow.Deskflow
-winget install -e --id Docker.DockerDesktop
-winget install -e --id DVDFlick.DVDFlick
-winget install -e --id Fastfetch-cli.Fastfetch
-winget install -e --id Google.GoogleDrive
-winget install -e --id MHNexus.HxD
-winget install -e --id NirSoft.IconsExtract
-winget install -e --id ImageMagick.ImageMagick
+Install-WinGetPackageClean -Id BlueStack.BlueStacks
+Install-WinGetPackageClean -Id DBBrowserForSQLite.DBBrowserForSQLite
+Install-WinGetPackageClean -Id Eassos.DiskGenius -InstallType SkipIfInstalled
+Install-WinGetPackageClean -Id Deskflow.Deskflow
+Install-WinGetPackageClean -Id Docker.DockerDesktop
+Install-WinGetPackageClean -Id DVDFlick.DVDFlick
+Install-WinGetPackageClean -Id Fastfetch-cli.Fastfetch
+Install-WinGetPackageClean -Id Google.GoogleDrive
+Install-WinGetPackageClean -Id MHNexus.HxD
+Install-WinGetPackageClean -Id NirSoft.IconsExtract
+Install-WinGetPackageClean -Id ImageMagick.ImageMagick
 
 # java
-if ((Get-WinGetPackage -Name JDK).Count -gt 1) { `
-    $Id = Get-WinGetPackage -Name JDK | Sort-Object Id | Select-Object -ExpandProperty Id | Select-Object -First 1; winget uninstall -e --id $Id `
-} `
-$Id = Find-WinGetPackage EclipseAdoptium.Temurin | Where-Object { $_.Version -match '^\d+(\.\d+)*$' } | Sort-Object -Property { [version]$_.Version } | Select-Object -Last 2 | Select-Object -First 1 -ExpandProperty Id; winget install -e --id $Id
-
-# jdk shortcut
+Install-WinGetPackageClean -Id EclipseAdoptium.Temurin -Like "*JDK*"
 $target = Get-ChildItem "C:\Program Files\Eclipse Adoptium" | `
     Sort-Object Name | `
     Select-Object -Last 1; `
@@ -59,46 +55,33 @@ if ($target) { `
     New-Item -ItemType SymbolicLink -Path "C:\jdk" -Target $target.FullName -Force `
 }
 
-winget install -e --id KDE.Krita
-winget install -e --id GuinpinSoft.MakeMKV
-
-# microsoft (c++) buildtools
-if ((Get-WinGetPackage -Name BuildTools).Count -gt 1) { `
-    $Id = Get-WinGetPackage -Name BuildTools | Sort-Object Id | Select-Object -ExpandProperty Id | Select-Object -First 1; winget uninstall -e --id $Id `
-} `
-$Id = Find-WinGetPackage BuildTools | Where-Object { $_.Version -match '^\d+(\.\d+)*$' } | Sort-Object -Property { [version]$_.Version } | Select-Object -Last 1 -ExpandProperty Id; winget install -e --id $Id
-
-winget install -e --id NextDNS.NextDNS
+Install-WinGetPackageClean -Id KDE.Krita
+Install-WinGetPackageClean -Id GuinpinSoft.MakeMKV
+Install-WinGetPackageClean -Id Microsoft.VisualStudio.BuildTools
+Install-WinGetPackageClean -Id NextDNS.NextDNS
 pipx upgrade openai-whisper 2>$null || pipx install openai-whisper
-winget install -e --id OpenJS.NodeJS.LTS
-
-# ollama
-if ((Get-WinGetPackage -Name "Ollama").Count -eq 0) { `
-	winget install -e --id Ollama.Ollama `
-}
-
-winget install -e --id Poly.PlantronicsHub
-winget install -e --id oschwartz10612.Poppler
-winget install -e --id Postman.Postman
-winget install -e --id PuTTY.PuTTY
+Install-WinGetPackageClean -Id OpenJS.NodeJS.LTS
+Install-WinGetPackageClean -Id Ollama.Ollama -InstallType SkipIfInstalled
+Install-WinGetPackageClean -Id Poly.PlantronicsHub
+Install-WinGetPackageClean -Id oschwartz10612.Poppler
+Install-WinGetPackageClean -Id Postman.Postman
+Install-WinGetPackageClean -Id PuTTY.PuTTY
 
 # python
-if ((Get-WinGetPackage Python.Python).Count -gt 1) { `
-    $Id = Get-WinGetPackage Python.Python | Sort-Object Id | Select-Object -ExpandProperty Id | Select-Object -First 1; winget uninstall -e --id $Id `
-} `
+Install-WinGetPackageClean -Id "Python.Python." -InstallType "UnknownId"
 python -m pip install --upgrade pip; `
 python -m pip install --user pipx; `
 python -m pipx ensurepath
 
-winget install -e --id RaspberryPiFoundation.RaspberryPiImager
-winget install -e --id Rufus.Rufus
-winget install -e --id SBCL.SBCL # steel bank common lisp
-winget install -e --id srjuddington.slade
-winget install -e --id SQLite.SQLite
-winget install -e --id UB-Mannheim.TesseractOCR
-winget install -e --id astral-sh.uv
-winget install -e --id Oracle.VirtualBox
-winget install -e --id WireGuard.WireGuard
+Install-WinGetPackageClean -Id RaspberryPiFoundation.RaspberryPiImager
+Install-WinGetPackageClean -Id Rufus.Rufus
+Install-WinGetPackageClean -Id SBCL.SBCL # steel bank common lisp
+Install-WinGetPackageClean -Id srjuddington.slade
+Install-WinGetPackageClean -Id SQLite.SQLite
+Install-WinGetPackageClean -Id UB-Mannheim.TesseractOCR
+Install-WinGetPackageClean -Id astral-sh.uv
+Install-WinGetPackageClean -Id Oracle.VirtualBox
+Install-WinGetPackageClean -Id WireGuard.WireGuard
 
 # wsl
 if (-Not (wsl --list -version)) { wsl --install }; `
@@ -109,7 +92,7 @@ wsl -d "Ubuntu" -u root -e do-release-upgrade; `
 wsl -d "Ubuntu" -u root -e apt autoremove -y; `
 wsl -d "Ubuntu" -u root -e apt clean -y
 
-winget install -e --id MikeFarah.yq
+Install-WinGetPackageClean -Id MikeFarah.yq
 pipx upgrade "yt-dlp[default]" 2>$null || pipx install "yt-dlp[default]"
 
 # config
