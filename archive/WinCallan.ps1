1c120e734e890e27111f5bf098e0a14a85f4322a
Chad Scribner <chadbscribner@gmail.com>
2026-08-13 22:19:24 -0500

    Remove unused files.

--- archive/WinCallan.ps1
+++
@@ -1,7 +0,0 @@
-# Non-package installations (add/update these first)
-
-. $PSScriptRoot\_WinCallanPrograms.ps1
-
-winget install -e --id NextDNS.NextDNS
-
-. $PSScriptRoot\_WinCallanCleanup.ps1
--- archive/WinDale.ps1
+++
@@ -1,4 +0,0 @@
-# Non-package installations (add/update these first)
-# (none)
-
-. $PSScriptRoot\_WinNiebPrograms.ps1
