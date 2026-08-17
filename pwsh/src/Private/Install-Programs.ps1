a5bf3a7ecab2692389e409249a0b50480aec7765
Chad Scribner <chadbscribner@gmail.com>
2026-08-17 09:02:07 -0500

    Remove powershell removal.

--- pwsh/src/Private/Install-Programs.ps1
+++ pwsh/src/Private/Install-Programs.ps1
@@ -27,8 +27,6 @@ choco feature enable -n='useRememberedArgumentsForUpgrades'
 choco upgrade chocolatey-font-helpers.extension -y -r -q --ignore-dependencies
 choco upgrade filezilla -y -r -q --ignore-dependencies
 choco upgrade nerd-fonts-meslo -y -r -q --ignore-dependencies
-choco uninstall powershell
-choco uninstall powershell-core
 
 # winget
 Install-WinGetPackageClean -Id cURL.cURL
