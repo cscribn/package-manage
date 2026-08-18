866b7be0a37515994cc657693660f6407946aa16
Chad Scribner <chadbscribner@gmail.com>
2026-08-17 20:56:33 -0500

    Use function.

--- pwsh/src/Private/Install-Apps.ps1
+++ pwsh/src/Private/Install-Apps.ps1
@@ -10,94 +10,116 @@ Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | ForEach-Object {Add-App
 Install-WinGetPackageClean -Id 9NP83LWLPZ9K # apple devices
 Install-WinGetPackageClean -Id 9PKTQ5699M62 # icloud
 
+function Remove-AppxPackagesFromSession {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string[]]$PackagePatterns
+    )
+
+    $Session = New-PSSession -UseWindowsPowerShell
+    try {
+        Invoke-Command -Session $Session -ScriptBlock {
+            param($Patterns)
+
+            foreach ($pattern in $Patterns) {
+                Get-AppxPackage $pattern | Remove-AppxPackage
+            }
+        } -ArgumentList ($PackagePatterns)
+    } finally {
+        if ($Session) {
+            $Session | Remove-PSSession
+        }
+    }
+}
+
 # remove apps; do all at once, due to https://github.com/PowerShell/PowerShell/issues/16652
-$Session = New-PSSession -UseWindowsPowerShell; Invoke-Command -Session $Session {
-    Get-AppxPackage *ACGMediaPlayer* | Remove-AppxPackage;
-    Get-AppxPackage *ActiproSoftwareLLC* | Remove-AppxPackage;
-    Get-AppxPackage *AdobeSystemsIncorporated.AdobePhotoshopExpress* | Remove-AppxPackage;
-    Get-AppxPackage *Amazon.com.Amazon* | Remove-AppxPackage;
-    Get-AppxPackage *AmazonVideo.PrimeVideo* | Remove-AppxPackage;
-    Get-AppxPackage *AppUp.IntelConnectivityPerformanceSuite* | Remove-AppxPackage;
-    Get-AppxPackage *AppUp.IntelGraphicsExperience* | Remove-AppxPackage;
-    Get-AppxPackage *AppUp.IntelManagementandSecurityStatus* | Remove-AppxPackage;
-    Get-AppxPackage *Asphalt8Airborne* | Remove-AppxPackage;
-    Get-AppxPackage *AutodeskSketchBook* | Remove-AppxPackage;
-    Get-AppxPackage *CaesarsSlotsFreeCasino* | Remove-AppxPackage;
-    Get-AppxPackage *Clipchamp.Clipchamp* | Remove-AppxPackage;
-    Get-AppxPackage *COOKINGFEVER* | Remove-AppxPackage;
-    Get-AppxPackage *CyberLinkMediaSuiteEssentials* | Remove-AppxPackage;
-    Get-AppxPackage *Disney* | Remove-AppxPackage;
-    Get-AppxPackage *DisneyMagicKingdoms* | Remove-AppxPackage;
-    Get-AppxPackage *Dolby* | Remove-AppxPackage;
-    Get-AppxPackage *DrawboardPDF* | Remove-AppxPackage;
-    Get-AppxPackage *Duolingo-LearnLanguagesforFree* | Remove-AppxPackage;
-    Get-AppxPackage *EclipseManager* | Remove-AppxPackage;
-    Get-AppxPackage *E046963F.cameraSettings* | Remove-AppxPackage;
-    Get-AppxPackage *Facebook* | Remove-AppxPackage;
-    Get-AppxPackage *FarmVille2CountryEscape* | Remove-AppxPackage;
-    Get-AppxPackage *fitbit* | Remove-AppxPackage;
-    Get-AppxPackage *Flipboard* | Remove-AppxPackage;
-    Get-AppxPackage *HiddenCity* | Remove-AppxPackage;
-    Get-AppxPackage *HULULLC.HULUPLUS* | Remove-AppxPackage;
-    Get-AppxPackage *iHeartRadio* | Remove-AppxPackage;
-    Get-AppxPackage *Instagram* | Remove-AppxPackage;
-    Get-AppxPackage *king.com.BubbleWitch3Saga* | Remove-AppxPackage;
-    Get-AppxPackage *LinkedInforWindows* | Remove-AppxPackage;
-    Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.3DBuilder* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.549981C3F5F10* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingFinance* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingFoodAndDrink* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingHealthAndFitness* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingNews* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingSearch* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingSports* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingTranslator* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingTravel* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.Messaging* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.Microsoft3DViewer* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.MicrosoftJournal* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.MicrosoftPowerBIForWindows* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.MixedReality.Portal* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.NetworkSpeedTest* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.News* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.Office.OneNote* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.Office.Sway* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.OneConnect* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.Print3D* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.SkypeApp* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.StartExperiencesApp* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.Todos* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.WindowsAlarms* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.WindowsFeedbackHub* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.WindowsSoundRecorder* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.XboxApp* | Remove-AppxPackage;
-    Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage;
-    Get-AppxPackage *MicrosoftCorporationII.MicrosoftFamily* | Remove-AppxPackage;
-    Get-AppxPackage *MirametrixInc.GlancebyMirametrix* | Remove-AppxPackage;
-    Get-AppxPackage *Netflix* | Remove-AppxPackage;
-    Get-AppxPackage *NYTCrossword* | Remove-AppxPackage;
-    Get-AppxPackage *OneCalendar* | Remove-AppxPackage;
-    Get-AppxPackage *PandoraMediaInc* | Remove-AppxPackage;
-    Get-AppxPackage *PhototasticCollage* | Remove-AppxPackage;
-    Get-AppxPackage *PicsArt-PhotoStudio* | Remove-AppxPackage;
-    Get-AppxPackage *Plex* | Remove-AppxPackage;
-    Get-AppxPackage *PolarrPhotoEditorAcademicEdition* | Remove-AppxPackage;
-    Get-AppxPackage *Royal Revolt* | Remove-AppxPackage;
-    Get-AppxPackage *Shazam* | Remove-AppxPackage;
-    Get-AppxPackage *Sidia.LiveWallpaper* | Remove-AppxPackage;
-    Get-AppxPackage *SlingTV* | Remove-AppxPackage;
-    Get-AppxPackage *Speed Test* | Remove-AppxPackage;
-    Get-AppxPackage *Spotify* | Remove-AppxPackage;
-    Get-AppxPackage *TikTok* | Remove-AppxPackage;
-    Get-AppxPackage *TuneInRadio* | Remove-AppxPackage;
-    Get-AppxPackage *Twitter* | Remove-AppxPackage;
-    Get-AppxPackage *Viber* | Remove-AppxPackage;
-    Get-AppxPackage *WinZipUniversal* | Remove-AppxPackage;
-    Get-AppxPackage *Wunderlist* | Remove-AppxPackage;
-    Get-AppxPackage *XING* | Remove-AppxPackage
-}; $Session | Remove-PSSession
+Remove-AppxPackagesFromSession -PackagePatterns @(
+    '*ACGMediaPlayer*',
+    '*ActiproSoftwareLLC*',
+    '*AdobeSystemsIncorporated.AdobePhotoshopExpress*',
+    '*Amazon.com.Amazon*',
+    '*AmazonVideo.PrimeVideo*',
+    '*AppUp.IntelConnectivityPerformanceSuite*',
+    '*AppUp.IntelGraphicsExperience*',
+    '*AppUp.IntelManagementandSecurityStatus*',
+    '*Asphalt8Airborne*',
+    '*AutodeskSketchBook*',
+    '*CaesarsSlotsFreeCasino*',
+    '*Clipchamp.Clipchamp*',
+    '*COOKINGFEVER*',
+    '*CyberLinkMediaSuiteEssentials*',
+    '*Disney*',
+    '*DisneyMagicKingdoms*',
+    '*Dolby*',
+    '*DrawboardPDF*',
+    '*Duolingo-LearnLanguagesforFree*',
+    '*EclipseManager*',
+    '*E046963F.cameraSettings*',
+    '*Facebook*',
+    '*FarmVille2CountryEscape*',
+    '*fitbit*',
+    '*Flipboard*',
+    '*HiddenCity*',
+    '*HULULLC.HULUPLUS*',
+    '*iHeartRadio*',
+    '*Instagram*',
+    '*king.com.BubbleWitch3Saga*',
+    '*LinkedInforWindows*',
+    '*MarchofEmpires*',
+    '*Microsoft.3DBuilder*',
+    '*Microsoft.549981C3F5F10*',
+    '*Microsoft.BingFinance*',
+    '*Microsoft.BingFoodAndDrink*',
+    '*Microsoft.BingHealthAndFitness*',
+    '*Microsoft.BingNews*',
+    '*Microsoft.BingSearch*',
+    '*Microsoft.BingSports*',
+    '*Microsoft.BingTranslator*',
+    '*Microsoft.BingTravel*',
+    '*Microsoft.BingWeather*',
+    '*Microsoft.Messaging*',
+    '*Microsoft.Microsoft3DViewer*',
+    '*Microsoft.MicrosoftJournal*',
+    '*Microsoft.MicrosoftOfficeHub*',
+    '*Microsoft.MicrosoftPowerBIForWindows*',
+    '*Microsoft.MicrosoftStickyNotes*',
+    '*Microsoft.MixedReality.Portal*',
+    '*Microsoft.NetworkSpeedTest*',
+    '*Microsoft.News*',
+    '*Microsoft.Office.OneNote*',
+    '*Microsoft.Office.Sway*',
+    '*Microsoft.OneConnect*',
+    '*Microsoft.Print3D*',
+    '*Microsoft.SkypeApp*',
+    '*Microsoft.StartExperiencesApp*',
+    '*Microsoft.Todos*',
+    '*Microsoft.WindowsAlarms*',
+    '*Microsoft.WindowsFeedbackHub*',
+    '*Microsoft.WindowsMaps*',
+    '*Microsoft.WindowsSoundRecorder*',
+    '*Microsoft.XboxApp*',
+    '*Microsoft.ZuneVideo*',
+    '*MicrosoftCorporationII.MicrosoftFamily*',
+    '*MirametrixInc.GlancebyMirametrix*',
+    '*Netflix*',
+    '*NYTCrossword*',
+    '*OneCalendar*',
+    '*PandoraMediaInc*',
+    '*PhototasticCollage*',
+    '*PicsArt-PhotoStudio*',
+    '*Plex*',
+    '*PolarrPhotoEditorAcademicEdition*',
+    '*Royal Revolt*',
+    '*Shazam*',
+    '*Sidia.LiveWallpaper*',
+    '*SlingTV*',
+    '*Speed Test*',
+    '*Spotify*',
+    '*TikTok*',
+    '*TuneInRadio*',
+    '*Twitter*',
+    '*Viber*',
+    '*WinZipUniversal*',
+    '*Wunderlist*',
+    '*XING*'
+) ]
