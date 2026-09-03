57d9164dafe91b82daa9ad13bc176a16268cafc6
Chad Scribner <chadbscribner@gmail.com>
2026-09-03 10:04:41 -0500

    Create install-chad.sh

---
+++ bash/ubunutu/bin/install-chad.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# global variables
+SCRIPT_NAME="$(basename "${0}")"
+readonly SCRIPT_NAME
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+readonly SCRIPT_DIR
+HOSTNAME="$(hostname)"
+readonly HOSTNAME
+
+# include
+source "${SCRIPT_DIR}/../lib/install-programs.sh"
+
+# autoremove, clean
+sudo apt-get autoremove -y; sudo apt-get clean -y
