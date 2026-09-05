6a2b390e806b1a2e025f70a5037b46e9c2d42de5
Chad Scribner <chadbscribner@gmail.com>
2026-09-05 16:41:02 -0500

    Add misc config.

--- bash/ubuntu/lib/copy-config.sh
+++ bash/ubuntu/lib/copy-config.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# ssh - enable
-sudo -A systemctl enable --now ssh
+# bash
+curl -sSLo "${HOME}/.bashrc" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/bash/bashrc-mac
+
+# oh-my-posh
+git_dir="${HOME}/.config/oh-my-posh"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull -q; cd -; else git clone -q "https://github.com/cscribn/dotfiles-oh-my-posh.git" "$git_dir"; fi
 
 # sleep, suspend, hibernate, hybrid-sleep - disable
 sudo -A systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# ssh - enable
+sudo -A systemctl enable --now ssh
 
 # laptop lid close actions - disable
 sudo -A sed -i 's/#\?HandleLidSwitch=.*/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
@@ -12,7 +18,7 @@ sudo -A sed -i 's/#\?HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPowe
 sudo -A sed -i 's/#\?HandleLidSwitchDocked=.*/HandleLidSwitchDocked=ignore/' /etc/systemd/logind.conf
 sudo -A systemctl restart systemd-logind
 
-# automatic login
+# login - automatic
 TARGET_USER="${SUDO_USER:-${USER:-$(whoami)}}"
 sudo -A mkdir -p /etc/lightdm/lightdm.conf.d/
 sudo -A sh -c 'cat <<EOF > /etc/lightdm/lightdm.conf.d/50-autologin.conf
@@ -20,3 +26,10 @@ sudo -A sh -c 'cat <<EOF > /etc/lightdm/lightdm.conf.d/50-autologin.conf
 autologin-user=${TARGET_USER}
 autologin-user-timeout=0
 EOF'
+
+# vim
+curl -sSLo "${HOME}/.vimrc" https://raw.githubusercontent.com/cscribn/dotfiles-misc/main/vim/vimrc
+
+# zsh
+git_dir="${HOME}/.config/zsh"; if [[ -d "$git_dir" ]]; then cd "$git_dir"; git pull -q; cd -; else git clone -q "https://github.com/cscribn/dotfiles-zsh.git" "$git_dir"; fi
+cp "${HOME}/.config/zsh/zshrc-mac" "${HOME}/.zshrc"
