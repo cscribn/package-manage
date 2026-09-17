13f88b0c32cbfb86b869b9f0bd6dfd847b689d8a
Chad Scribner <chadbscribner@gmail.com>
2026-09-17 07:28:23 -0500

    Fix docker install.

--- bash/ubuntu/lib/install-batocera-env.sh
+++ bash/ubuntu/lib/install-batocera-env.sh
@@ -6,8 +6,36 @@ if [[ -n "${SUDO_ASKPASS:-}" ]]; then
 fi
 
 apt_run install build-essential -y
-apt_run install docker.io -y
 
+
+# docker
+apt_run install ca-certificates -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+CONTENT=$(cat <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+)
+
+TARGET_FILE="/etc/apt/sources.list.d/docker.sources"
+
+if [ ! -f "$TARGET_FILE" ] || [ "$(cat "$TARGET_FILE")" != "$CONTENT" ]; then
+    echo "$CONTENT" | sudo tee "$TARGET_FILE" > /dev/null
+fi
+
+sudo apt update
+apt_run install docker-ce -y
+apt_run install docker-ce-cli -y
+apt_run install containerd.io -y
+apt_run install docker-buildx-plugin -y
+apt_run install docker-compose-plugin -y
 sudo groupadd -f docker
 
 if ! id -nG "$USER" | grep -qw "docker"; then
